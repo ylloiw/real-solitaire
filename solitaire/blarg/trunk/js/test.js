2ae8f8746e61fe98cc91b9efc3c28ebbf028140c
ylloiw <71865888+ylloiw@users.noreply.github.com>
2023-07-10 22:52:57 -0400

    Init

---
+++ solitaire/blarg/trunk/js/test.js
@@ -0,0 +1,34 @@
+function runTest() {
+	var gameBoard=new Board("Klondike");
+	var deck=new Deck(defaultType);
+	deck.setX(1);
+	deck.setY(1);
+	gameBoard.addDeck(deck);
+	deck.addTop(new Card(5,"s"));
+	deck.addTop(new Card(6,"s"));
+	var deck2=new Deck(defaultType);
+	deck2.setX(2);
+	deck2.setY(1);
+	gameBoard.addDeck(deck2);
+	var card=new Card(1,"h");
+	card.setFaceUp(false);
+	deck2.addTop(card);
+	deck2.addTop(card);
+	deck2.addTop(card);
+	deck2.addTop(card);
+	deck2.addTop(card);
+	deck2.addTop(card);
+	deck2.addTop(card);
+	deck2.addTop(card);
+	deck2.addTop(card);
+	deck2.addTop(card);
+	deck2.addTop(card);
+	deck2.addTop(card);
+	deck2.addTop(card);
+	deck2.addTop(new Card(3,"s"));
+	var deck3=new Deck(collapsedType);
+	deck3.setX(2);
+	deck3.setY(2);
+	gameBoard.addDeck(deck3);
+	deck3.addTop(new Card(9,"h"));
+}
